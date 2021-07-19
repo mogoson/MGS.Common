c4711939d88eb9719ed1cc95c09aa9b5dc02506a
Mogoson <mogoson@outlook.com>
2021-07-20 00:06:04 +0800

    Add demo files

---
+++ UnityProject/Assets/MGS.Packages/Common/Demo/Scripts/DispatcherDemo.cs
@@ -0,0 +1,52 @@
+﻿/*************************************************************************
+ *  Copyright (c) #COPYRIGHTYEAR# Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  DispatcherDemo.cs
+ *  Description  :  Null.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  0.1.0
+ *  Date         :  #CREATEDATE#
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MGS.Common.Threading.Demo
+{
+    public class DispatcherDemo : MonoBehaviour
+    {
+        [SerializeField]
+        Image image;
+
+        bool isLoop = true;
+
+        void Start()
+        {
+            var thread = new Thread(() =>
+            {
+                while (isLoop)
+                {
+                    Dispatcher.BeginInvoke(() =>
+                    {
+                        //Run code on main thread.
+                        var r = Random.Range(0, 1.0f);
+                        var g = Random.Range(0, 1.0f);
+                        var b = Random.Range(0, 1.0f);
+                        image.color = new Color(r, g, b);
+                    });
+                    Thread.Sleep(1000);
+                }
+            })
+            { IsBackground = true };
+            thread.Start();
+        }
+
+        void OnDestroy()
+        {
+            isLoop = false;
+        }
+    }
+}
---
+++ UnityProject/Assets/MGS.Packages/Common/Demo/Scripts/JsonUtilityProDemo.cs
@@ -0,0 +1,45 @@
+﻿/*************************************************************************
+ *  Copyright (c) #COPYRIGHTYEAR# Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  JsonUtilityProDemo.cs
+ *  Description  :  Null.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  0.1.0
+ *  Date         :  #CREATEDATE#
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MGS.Common.Serialization.Demo
+{
+    public class JsonUtilityProDemo : MonoBehaviour
+    {
+        void Start()
+        {
+            var testList = new List<string>()
+            {
+                "A","BB","CCC"
+            };
+
+            var json = JsonUtilityPro.ToJson(testList);
+            Debug.LogFormat("List to json is {0}", json);
+
+            var list = JsonUtilityPro.FromJson<string>(json);
+            Debug.LogFormat("List from json, item count is {0}", list.Count);
+
+            var testDic = new Dictionary<int, string>()
+            {
+                { 0,"A"},{1,"BB" },{2,"CCC" }
+            };
+
+            var dicJson = JsonUtilityPro.ToJson(testDic);
+            Debug.LogFormat("Dictionary to json is {0}", dicJson);
+
+            var dictionary = JsonUtilityPro.FromJson<int, string>(dicJson);
+            Debug.LogFormat("Dictionary from json, item count is {0}", dictionary.Count);
+        }
+    }
+}
